a5fc982ebe9c97029e63857d360b1bc7db47c099
joonhee305 <74956803+joonhee305@users.noreply.github.com>
2023-03-23 22:11:17 +0900

    javaitsmine

--- KyuChul/untitled/src/feat_3.java
+++ KyuChul/untitled/src/feat_3.java
@@ -2,7 +2,8 @@ public class feat_3 {
 
     public static void main(String[] args){
 
-        System.out.println("신규철의 깃 테스트 첫번째 입니다.3번쨰 시도입니다. merge후 삭제 하지 않은 feat_3의 수정후 재 push입니다.");
+        System.out.println("신규철의 깃 테스트 첫번째 입니다.3번쨰 시도입니다. 응 수정했어 수고해");
+        System.out.println("이 자바 파일은 이제 제껍니다.");
 
     }
 
